956c719df0259003dab661ca403beb7375d1ec0f
Enrico <73340659+Enrypase@users.noreply.github.com>
2022-03-22 21:17:10 +0100

    Update IUtils_bys.sol

--- Interfaces/IUtils_bys.sol
+++ Interfaces/IUtils_bys.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.0;
 
 interface Utils_bys{
     function toString(uint value) external returns (string memory);
-    // Does a string contain another one?
+    // Contains function. Returns true if a specified string contains another specified string
     function contains(string memory main, string memory subString) external pure returns (bool);
     // Inspired by: https://ethereum.stackexchange.com/questions/52246/solidity-extracting-slicing-characters-from-a-string
+    // Returns the "slice" (substring) of a string
     function getSlice(uint begin, uint end, string memory text) external pure returns (string memory);
-    // Is a string equal to another?
+    // Returns true if a string is equal to another
     function equals(string memory a, string memory b) external pure returns (bool);
 }
